c91addd8b3919a6347dec2e67b3ac31c8be9b557
Lucas Trevizan <72326473+LucasTrevizanbr@users.noreply.github.com>
2022-06-13 15:28:39 -0300

    Adicionando conteúdo sobre servelets

---
+++ Servlet/Código/gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+	
+	private static List<Empresa> lista = new ArrayList<>();
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		lista.add(empresa);
+		lista.add(empresa2);
+	}
+
+	public void adiciona(Empresa empresa) {
+		Banco.lista.add(empresa);
+	}
+	
+	public List<Empresa> getEmpresas(){
+		return Banco.lista;
+	}
+
+}
